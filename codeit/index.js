6b010211e6c94c642914c784b64c45f8f7741d9b
Tasha <lustllz@gmail.com>
2023-02-03 10:57:41 +0900

    object oriented fundamental 4 concepts

--- codeit/index.js
+++ codeit/index.js
@@ -8,11 +8,35 @@ const user = {
 } //Object Literal
 
 function createUser(email, birthdate){
+    const _email = email
+    let _point = 0
+
+    function increasePoint(){
+        _point +=1
+    }
+
     const user = {
-        email,
         birthdate,
+
+        get email(){
+            return _email
+        },
+
+        get point(){
+            return _point
+        },
+
+        set email(address){
+            if(address.includes('@')){
+                _email = address
+            }else{
+                throw new Error('invalid email address')
+            }
+        },
+
         buy(item){
             console.log(`${this.email} buys ${item.name}`)
+            increasePoint()
         }
     }
     return user
@@ -35,7 +59,36 @@ class User{
     buy(item){
         console.log(`${this.email} buys ${item.name}`)
     }
-} //Class
+
+    get email(){
+        return this._email
+    }
+
+    set email(address){
+        if(address.includes('@')){
+            this._email = address
+        }else{
+            throw new Error('invalid email address')
+        }
+    }
+} //Parent Class
+
+class PremiumUser extends User{
+    constructor(email, birthdate, level, point){
+        super(email, birthdate)
+        this.level = level
+        this.point = point
+    }
+
+    buy(item){
+        super.buy(item)
+        this.point += item.price * 0.05
+    }
+
+    streamMusicForFree(){
+        console.log(`Free music streaming for ${this.email}`)
+    }
+} //Child Class
 
 const item = {
     name: '스웨터',
@@ -45,7 +98,10 @@ const item = {
 const user1 = new User('chris123@google.com', '1992-03-21')
 const user2 = createUser('jerry99@google.com', '1995-07-19')
 const user3 = createUser('alice@google.com', '1993-12-24')
+const pUser1 = new PremiumUser('chris123@google.com', '1992-03-21', 3)
 
 console.log(user.email)
 console.log(user.birthdate)
 user.buy(item)
+user1.buy(item)
+pUser1.buy(item)
