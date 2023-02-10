fa496652a4dff57d85f1d8feb9affdf038e2df9b
Tasha <lustllz@gmail.com>
2023-02-10 17:09:00 +0900

    Seperate modules

--- codeit/index.js
+++ codeit/index.js
@@ -105,3 +105,20 @@ console.log(user.birthdate)
 user.buy(item)
 user1.buy(item)
 pUser1.buy(item)
+
+class Math{
+    static PI = 3.14
+
+    static getCircleArea(radius){
+        return Math.PI * radius * radius
+    }
+}
+
+Math.PI = 3.141592
+Math.getRectangleArea = function (width, height){
+    return width * height
+}
+
+console.log(Math.PI)
+console.log(Math.getCircleArea(5))
+console.log(Date.now())
