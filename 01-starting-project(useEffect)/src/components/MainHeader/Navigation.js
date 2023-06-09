ee8fb7abe32423061bc68d4044170df2124bdbe1
Tasha <lustllz@gmail.com>
2023-06-09 17:39:20 +0900

    dynamic useContext & useImperativeHandle

--- 01-starting-project(useEffect)/src/components/MainHeader/Navigation.js
+++ 01-starting-project(useEffect)/src/components/MainHeader/Navigation.js
@@ -3,24 +3,24 @@ import React, { useContext } from "react";
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
-const Navigation = (props) => {
-    const ctx = useContext(AuthContext);
+const Navigation = () => {
+    const { isLoggedIn, onLogout } = useContext(AuthContext);
     return (
         <nav className={classes.nav}>
             <ul>
-                {ctx.isLoggedIn && (
+                {isLoggedIn && (
                     <li>
                         <a href="/">Users</a>
                     </li>
                 )}
-                {ctx.isLoggedIn && (
+                {isLoggedIn && (
                     <li>
                         <a href="/">Admin</a>
                     </li>
                 )}
-                {ctx.isLoggedIn && (
+                {isLoggedIn && (
                     <li>
-                        <button onClick={props.onLogout}>Logout</button>
+                        <button onClick={onLogout}>Logout</button>
                     </li>
                 )}
             </ul>
