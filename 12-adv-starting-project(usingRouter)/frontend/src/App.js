7012073a415fe34eefc1e941970bcdadbb8cc72b
Tasha <lustellz@gmail.com>
2023-09-12 18:13:56 +0900

    backup project for navigator

--- 12-adv-starting-project(usingRouter)/frontend/src/App.js
+++ 12-adv-starting-project(usingRouter)/frontend/src/App.js
@@ -20,8 +20,41 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            {
+                path: "events",
+                element: <EventsRootLayout />,
+                children: [
+                    {
+                        index: true,
+                        element: <EventsPage />,
+                        loader: eventsLoader, // to make the file leaner, we had moved loader function to other place(which is logically more proper)
+                    }, // loader is a function, that would be performed whenever the router to be called
+                    { path: ":eventId", element: <EventDetailPage /> },
+                    { path: "new", element: <NewEventPage /> },
+                    { path: ":eventId/edit", element: <EditEventPage /> },
+                ],
+            },
+        ],
+    },
+]);
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
