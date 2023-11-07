56366592a2ce8b6d63e0cc8b8862788c145406cb
Tasha <lustellz@gmail.com>
2023-11-07 17:54:03 +0900

    finishing useState & useEffect & useCallback

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
+import LoadingIndicator from "../UI/LoadingIndicator";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-    const [inputState, setInputState] = useState({ title: "", amount: "" });
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+
     const submitHandler = (event) => {
+        // we can't use hooks here
         event.preventDefault();
         // ...
+
+        props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
     };
 
     return (
@@ -19,13 +25,9 @@ const IngredientForm = React.memo((props) => {
                         <input
                             type="text"
                             id="title"
-                            value={inputState.title}
+                            value={enteredTitle}
                             onChange={(event) => {
-                                const newTitle = event.target.value;
-                                setInputState((prevInputState) => ({
-                                    title: newTitle,
-                                    amount: prevInputState.amount,
-                                }));
+                                setEnteredTitle(event.target.value);
                             }}
                         />
                     </div>
@@ -34,18 +36,15 @@ const IngredientForm = React.memo((props) => {
                         <input
                             type="number"
                             id="amount"
-                            value={inputState.amount}
+                            value={enteredAmount}
                             onChange={(event) => {
-                                const newAmount = event.target.value;
-                                setInputState((prevInputState) => ({
-                                    amount: newAmount,
-                                    title: prevInputState.title,
-                                }));
+                                setEnteredAmount(event.target.value);
                             }}
                         />
                     </div>
                     <div className="ingredient-form__actions">
                         <button type="submit">Add Ingredient</button>
+                        {props.loading && <LoadingIndicator />}
                     </div>
                 </form>
             </Card>
--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,90 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
+import Search from "./Search";
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
+    const [userIngredients, setUserIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
-}
+    const filteredIngredientsHandler = useCallback(
+        (filteredIngredients) => {
+            setUserIngredients(filteredIngredients);
+        },
+        [setUserIngredients]
+    );
+
+    const addIngredientHandler = (ingredient) => {
+        setIsLoading(true);
+        fetch(
+            "https://react-http-da86f-default-rtdb.firebaseio.com/ingredients.json",
+            {
+                method: "POST",
+                body: JSON.stringify(ingredient),
+                headers: { "Content-Type": "application/json" },
+            }
+        )
+            .then((response) => {
+                setIsLoading(false);
+                return response.json();
+            })
+            .then((responseData) => {
+                setUserIngredients((prevIngredients) => [
+                    ...prevIngredients,
+                    { id: responseData.name, ...ingredient },
+                ]);
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    const removeIngredientHandler = (ingredientId) => {
+        setIsLoading(true);
+        fetch(
+            `https://react-http-da86f-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json/`,
+            {
+                method: "DELETE",
+            }
+        )
+            .then((response) => {
+                setIsLoading(false);
+                setUserIngredients((prevIngredients) =>
+                    prevIngredients.filter(
+                        (ingredient) => ingredient.id !== ingredientId
+                    )
+                );
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    const clearError = () => {
+        setError(null);
+        setIsLoading(false);
+    };
+
+    return (
+        <div className="App">
+            {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+            <IngredientForm
+                onAddIngredient={addIngredientHandler}
+                loading={isLoading}
+            />
+
+            <section>
+                <Search onLoadIngredients={filteredIngredientsHandler} />
+                <IngredientList
+                    ingredients={userIngredients}
+                    onRemoveItem={removeIngredientHandler}
+                />
+            </section>
+        </div>
+    );
+};
 
 export default Ingredients;
