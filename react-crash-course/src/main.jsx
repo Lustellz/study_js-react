36f7874b0a2f41cd926554e33f0bdbcb99ab7e27
Tasha <lustellz@gmail.com>
2023-12-27 17:29:22 +0900

    backup crush course

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Posts, { loader as postsLoader } from "./routes/Posts";
+import "./index.css";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
+import RootLayout from "./routes/RootLayout";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const router = createBrowserRouter([
+    // route defeinitions
+    {
+        path: "/",
+        element: <RootLayout />, // layout route
+        children: [
+            {
+                path: "/", // main domain
+                element: <Posts />,
+                loader: postsLoader,
+                children: [
+                    {
+                        path: "/create-post",
+                        element: <NewPost />,
+                        action: newPostAction,
+                    },
+                    {
+                        path: "/:id",
+                        element: <PostDetails />,
+                        loader: postDetailsLoader,
+                    },
+                ],
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
